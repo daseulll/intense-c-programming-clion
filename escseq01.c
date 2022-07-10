e515ead56b5d4840d0cb420f69a5d89b84d9a7e0
daseulll <nldaseul@gmail.com>
2022-07-10 16:20:32 +0900

    Pratice printf() that is combination stdout function with format string consist of format char, escape sequence, strings

---
+++ escseq01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 3;
+    printf("x는 %f 입니다.\n", x);
+
+    char greeting[6] = "hello";
+    printf("\t %s \n", greeting);
+
+    printf("%c\n", 'D');
+}
